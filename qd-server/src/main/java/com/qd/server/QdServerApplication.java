b74d01b4699cf89e2b17bb0489f87235b62688c6
18301069135 <422041086@qq.com>
2021-01-21 18:50:15 +0800

    框架搭建

--- qd-server/src/main/java/com/qd/server/QdServerApplication.java
+++ qd-server/src/main/java/com/qd/server/QdServerApplication.java
@@ -2,12 +2,15 @@ package com.qd.server;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
+@ComponentScan(basePackages = { "com.qd.server.*" })
 public class QdServerApplication {
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(QdServerApplication.class).run(args);
+		new SpringApplicationBuilder(QdServerApplication.class)
+				.properties("spring.config.location=classpath:/application.yml").run(args);
 	}
 }
